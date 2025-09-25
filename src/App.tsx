3b732cec1bceb7843d6a24fe07246d701d774936
Salomon-Okombi <salomon.okombi@gmail.com>
2025-09-26 00:36:01 +0200

    Initialisation projet frontend avec Vite + React + Tailwind + DaisyUI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Home } from "lucide-react";
+
+function App() {
+  return (
+    <>
+      <button className="m-4 btn"><Home>w-4 h-5 </Home>Button</button>
+    </>
+  );
+}
+
+export default App;
